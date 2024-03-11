c925e287ee1de84483a726bf5093719b437e045a
허승경 <hhe98hsk@gmail.com>
2024-03-11 17:52:38 +0900

    [Setting] Funding 컴포넌트에서 컨벤션 설정

--- funding/src/main/java/com/ssafy/movie/MovieApplication.java
+++ funding/src/main/java/com/ssafy/movie/MovieApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MovieApplication.class, args);
+    }
 }
