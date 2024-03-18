de89b15d23aab37165369be321ea4a2feae2bc58
minjeong <a50070863@gmail.com>
2024-03-18 17:58:08 +0900

    feat: 배치 어노테이션 추가 (#51)

--- back/batch/src/main/java/com/ssafy/batch/BatchApplication.java
+++ back/batch/src/main/java/com/ssafy/batch/BatchApplication.java
@@ -1,8 +1,11 @@
 package com.ssafy.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
 
