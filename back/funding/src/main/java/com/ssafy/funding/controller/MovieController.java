677a16d7c359ea9f53e60e18af2d67ce41754457
임승연 <lsy5490@naver.com>
2024-03-25 01:09:56 +0900

    [Feature] 실시간 인기 영화 순위 API 개발

--- back/funding/src/main/java/com/ssafy/funding/controller/MovieController.java
+++ back/funding/src/main/java/com/ssafy/funding/controller/MovieController.java
@@ -1,15 +1,14 @@
 package com.ssafy.funding.controller;
 
 import com.ssafy.funding.domain.Movie;
+import com.ssafy.funding.dto.DetailMovie;
 import com.ssafy.funding.service.MovieService;
 import io.swagger.v3.oas.annotations.Operation;
+import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/movies")
@@ -22,5 +21,17 @@ public class MovieController {
     @GetMapping("/search")
     public ResponseEntity<List<Movie>> searchMovie(@RequestParam String word) {
         return ResponseEntity.ok(movieService.searchMovie(word));
+    }
+
+    @Operation(summary = "검색을 통해 영화(펀딩) 상세 정보를 조회합니다.")
+    @GetMapping("/{movieId}")
+    public ResponseEntity<Movie> detailMovieBySearch(@PathVariable int movieId) throws IOException {
+        return ResponseEntity.ok(movieService.detailMovieBySearch(movieId));
+    }
+
+    @Operation(summary = "실시간 인기 영화를 조회합니다.")
+    @GetMapping("/popular")
+    public ResponseEntity<List<DetailMovie>> popularMovies() throws IOException {
+        return ResponseEntity.ok(movieService.popularMovies());
     }
 }
