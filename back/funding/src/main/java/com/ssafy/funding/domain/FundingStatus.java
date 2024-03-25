0243dca880c6f2837fdd4429bd5b6325b6153023
강민정 <a50070863@gmail.com>
2024-03-25 14:41:27 +0900

    [Feature] 결제 승인 로직 수정

--- back/funding/src/main/java/com/ssafy/funding/domain/FundingStatus.java
+++ back/funding/src/main/java/com/ssafy/funding/domain/FundingStatus.java
@@ -8,9 +8,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum FundingStatus {
     // 참여 가능한 펀딩
-    OPEN("OPEN", "오픈"),
-    CLOSED("CLOSED", "종료"),
-    WAITING("WAITING", "대기");
+    NONE("NONE", "무딩 준비 중"),
+    READY_TO_OPEN("READY_TO_OPEN", "무딩 예정"),
+    OPEN("OPEN", "무딩 중"),
+    READY_TO_RESERVATION("READY_TO_RESERVATION", "예매 예정"),
+    RESERVATION("RESERVATION", "예매 진행"),
+    CLOSED("CLOSED", "무딩 종료");
 
     private String value;
 
--- back/funding/src/main/java/com/ssafy/funding/service/OrderService.java
+++ back/funding/src/main/java/com/ssafy/funding/service/OrderService.java
@@ -1,19 +1,14 @@
 package com.ssafy.funding.service;
 
 import static com.ssafy.funding.exception.global.CustomExceptionStatus.FUNDING_NOT_FOUND;
-import static com.ssafy.funding.exception.global.CustomExceptionStatus.ORDER_NOT_FOUND;
 
 import com.ssafy.funding.domain.Funding;
 import com.ssafy.funding.domain.Order;
-import com.ssafy.funding.domain.PaymentMethod;
 import com.ssafy.funding.domain.validator.OrderValidator;
 import com.ssafy.funding.dto.Money;
 import com.ssafy.funding.dto.request.ConfirmOrderRequest;
 import com.ssafy.funding.dto.request.ConfirmPaymentsRequest;
-import com.ssafy.funding.dto.request.CreateOrderRequest;
-import com.ssafy.funding.dto.request.CreatePaymentsRequest;
 import com.ssafy.funding.dto.response.OrderConfirmResponse;
-import com.ssafy.funding.dto.response.OrderResponse;
 import com.ssafy.funding.exception.BadRequestException;
 import com.ssafy.funding.repository.FundingRepository;
 import com.ssafy.funding.repository.OrderRepository;
@@ -25,56 +20,45 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional
-// @Transactional(readOnly = true)
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final FundingRepository fundingRepository;
     private final OrderValidator orderValidator;
     private final PaymentClient paymentClient;
-    // private final Producer producer;
 
-    // @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    // @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public OrderResponse orderFunding(final CreateOrderRequest createOrderRequest, Integer userId) {
-        final Order order = createFundingOrder(createOrderRequest, userId);
+    public Order orderFunding(final ConfirmOrderRequest confirmOrderRequest, Integer userId) {
+        final Order order = createFundingOrder(confirmOrderRequest, userId);
         order.confirm(1, orderValidator);
-        paymentClient.callTossPay(
-                CreatePaymentsRequest.builder()
-                        .id(order.getId())
-                        .orderId(order.getUuid()) // 주문 번호
-                        .method(PaymentMethod.of(createOrderRequest.getMethod()).getMethod())
-                        .amount(
-                                Long.valueOf(
-                                        order.getCount() * order.getPrice())) // 단일 펀딩을 다수 개 주문 가능
-                        .orderName("주문")
-                        .successUrl("http://localhost:8082")
-                        .failUrl("http://localhost:8082")
-                        .build());
-        // producer.sendCreateOrderRequest("order", order);
-        return OrderResponse.of(order);
+        return order;
     }
 
     public Order createFundingOrder(
-            final CreateOrderRequest createOrderRequest, final Integer currentUserId) {
+            final ConfirmOrderRequest confirmOrderRequest, final Integer currentUserId) {
         final Funding funding =
                 fundingRepository
-                        .findById(createOrderRequest.getFundingId())
+                        .findById(confirmOrderRequest.getFundingId())
                         .orElseThrow(() -> new BadRequestException(FUNDING_NOT_FOUND));
 
         final Order order =
                 Order.createPaymentOrder(
                         currentUserId,
                         funding,
-                        createOrderRequest.getFundingCount(),
+                        confirmOrderRequest.getFundingCount(),
                         funding.getPrice(),
                         orderValidator);
 
         return orderRepository.save(order);
     }
 
+    /*
+    결제 승인
+     */
     public OrderConfirmResponse confirmOrder(
             String orderUuid, ConfirmOrderRequest confirmOrderRequest, Integer userId) {
+
+        Order order = orderFunding(confirmOrderRequest, userId); // 주문 생성
+
         ConfirmPaymentsRequest confirmPaymentsRequest =
                 ConfirmPaymentsRequest.builder()
                         .paymentKey(confirmOrderRequest.getPaymentKey())
@@ -82,15 +66,11 @@ public class OrderService {
                         .orderId(orderUuid)
                         .build();
 
-        Order order =
-                orderRepository
-                        .findByUuid(confirmPaymentsRequest.getOrderId())
-                        .orElseThrow(() -> new BadRequestException(ORDER_NOT_FOUND));
         Money paymentWons = Money.wons(confirmPaymentsRequest.getAmount());
 
-        orderValidator.validOwner(order, userId);
-        orderValidator.validMethodIsPaymentOrder(order);
-        orderValidator.validAmountIsSameAsRequest(order, paymentWons);
+        // orderValidator.validOwner(order, userId);
+        // orderValidator.validMethodIsPaymentOrder(order);
+        // orderValidator.validAmountIsSameAsRequest(order, paymentWons);
 
         paymentClient.callTossPayConfirm(confirmPaymentsRequest, order.getId());
 
