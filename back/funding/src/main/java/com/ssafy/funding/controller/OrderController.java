2e6ae6693569e17d331987d6ec991604fbe478ab
강민정 <a50070863@gmail.com>
2024-03-26 21:04:38 +0900

    [Feature] 결제 요청 테스트 API 추가

--- back/funding/src/main/java/com/ssafy/funding/controller/OrderController.java
+++ back/funding/src/main/java/com/ssafy/funding/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.ssafy.funding.controller;
 
 import com.ssafy.funding.dto.request.ConfirmOrderRequest;
+import com.ssafy.funding.dto.request.CreatePaymentsRequest;
 import com.ssafy.funding.service.OrderService;
 import com.ssafy.funding.service.client.UserClient;
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,15 +17,12 @@ public class OrderController {
     private final OrderService orderService;
     private final UserClient userClient;
 
-    //    @Operation(summary = "주문(펀딩)을 결제합니다.")
-    //    @PostMapping
-    //    public ResponseEntity<?> orderFunding(@RequestBody CreateOrderRequest createOrderRequest)
-    // {
-    //        return ResponseEntity.ok(
-    //                orderService.orderFunding(
-    //                        createOrderRequest,
-    //                        Integer.parseInt(userClient.findUserInfo().getUsername())));
-    //    }
+    @Operation(summary = "주문(펀딩)을 결제합니다.")
+    @PostMapping
+    public ResponseEntity<?> orderFunding(
+            @RequestBody CreatePaymentsRequest createPaymentsRequest) {
+        return ResponseEntity.ok(orderService.requestPayment(createPaymentsRequest));
+    }
 
     @Operation(summary = "결제 확인하기. successUrl 로 돌아온 웹페이지에서 query 로 받은 응답값을 서버로 보내주세요.")
     @PostMapping("/{order_uuid}/confirm")
