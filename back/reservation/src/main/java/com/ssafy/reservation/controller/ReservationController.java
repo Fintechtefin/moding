4018c191b350158f9f2ffb157716755a9ec86257
허승경 <hhe98hsk@gmail.com>
2024-03-28 16:11:40 +0900

    [Feature] 좌석 예매 CORS 설정

--- back/reservation/src/main/java/com/ssafy/reservation/controller/ReservationController.java
+++ back/reservation/src/main/java/com/ssafy/reservation/controller/ReservationController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 // @RequestMapping("/reservation")
