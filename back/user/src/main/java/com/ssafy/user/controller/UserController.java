1a0622bbfa3159b416f27cdb52b5188ecc30cae3
임승연 <lsy5490@naver.com>
2024-03-30 01:30:12 +0900

    [Feature] 애프터무딩 api 개발

--- back/user/src/main/java/com/ssafy/user/controller/UserController.java
+++ back/user/src/main/java/com/ssafy/user/controller/UserController.java
@@ -3,12 +3,11 @@ package com.ssafy.user.controller;
 import static com.ssafy.user.util.AuthenticationUtil.getCurrentUserSocialId;
 
 import com.ssafy.user.service.LoginService;
+import com.ssafy.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -17,10 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final LoginService loginService;
+    private final UserService userService;
 
+    @Operation(description = "로그아웃")
     @DeleteMapping("/logout")
     public ResponseEntity<?> logout() {
         loginService.logout(getCurrentUserSocialId());
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(description = "내가 참여한 펀딩 결과 조회 (애프터 무딩)")
+    @GetMapping("/fundings/result")
+    public ResponseEntity<?> getMyFundingResult(@RequestHeader("accessToken") String accessToken) {
+        return ResponseEntity.ok().body(userService.getMyFundingResult(accessToken));
     }
 }
--- back/user/src/main/java/com/ssafy/user/repository/UserRepository.java
+++ back/user/src/main/java/com/ssafy/user/repository/UserRepository.java
@@ -1,9 +1,43 @@
 package com.ssafy.user.repository;
 
 import com.ssafy.user.domain.User;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    public interface AfterMoodingResponseInterface {
+        int getMovieId();
+
+        String getTitle();
+
+        String getPoster();
+
+        LocalDate getDate();
+
+        int getAttendCnt();
+
+        int getGoalCnt();
+
+        int getReservationId();
+
+        int getFundingFinalResult();
+    }
+
     Optional<User> findBySocialId(String socialLoginId);
+
+    @Query(
+            value =
+                    "select movie.movie_id movieId, movie.title, movie.poster, funding.date,"
+                            + "(select sum(count) from orders where orders.funding_id=funding.funding_id) attendCnt,"
+                            + "(select reservation_id from reservation where reservation.funding_id=funding.funding_id) reservationId,"
+                            + "(select funding_final_result from funding_history where funding_history.funding_id=funding.funding_id) fundingFinalResult,"
+                            + "funding.people_count goalCnt from funding "
+                            + "join movie on movie.movie_id=funding.movie_id "
+                            + "join orders on orders.funding_id=funding.funding_id where orders.user_id=:userId and movie.status='CLOSED' group by funding.funding_id order by funding.funding_id desc",
+            nativeQuery = true)
+    List<AfterMoodingResponseInterface> getMyFundingResult(int userId);
 }
