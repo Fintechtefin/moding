ee259b68209cfa4fbd3fb6ad5c09656f94464a73
정태윤 <wjdxodbs52@naver.com>
2024-04-04 04:43:21 +0900

    [Feature] 로그아웃 API 연결

--- frontEnd/src/components/myPage/OptionItem.tsx
+++ frontEnd/src/components/myPage/OptionItem.tsx
@@ -1,23 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
 import { MenuOptionItem } from "@util/types";
+import { axiosApi } from "@util/commons";
 
 interface Props extends MenuOptionItem {}
 
-const OptionItem = ({ name, url, disable }: Props) => {
+const OptionItem = ({ name, type, url, disable }: Props) => {
   const navigate = useNavigate();
 
-  return (
+  const logout = async () => {
+    try {
+      const res = await axiosApi().delete("users/user/logout");
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return disable ? (
+    <div className="flex justify-between cursor-not-allowed">
+      <div>{name}</div>
+      <div className="flex items-center px-[1vh] text-[2vh] border-red-600 border text-red-600 rounded">
+        추후 공개
+      </div>
+    </div>
+  ) : (
     <div
       className="flex justify-between cursor-pointer"
-      onClick={() => navigate(url)}
+      onClick={() => (type === "navigate" ? navigate(url) : logout())}
     >
       <div>{name}</div>
-      {!disable && (
-        <div className="flex items-center gap-[1.5vh] text-[2.5vh]">
-          <IoIosArrowForward />
-        </div>
-      )}
+      <div className="flex items-center text-[2.5vh]">
+        <IoIosArrowForward />
+      </div>
     </div>
   );
 };
