0958ca02e81df8681955acd31bec93707d5226fd
강민정 <a50070863@gmail.com>
2024-06-30 12:07:20 +0900

    test: funding 컨텍스트 로드 테스트 비활성화 (#4)

--- back/funding/src/test/java/com/ssafy/funding/FundingApplicationTests.java
+++ back/funding/src/test/java/com/ssafy/funding/FundingApplicationTests.java
@@ -2,9 +2,8 @@ package com.ssafy.funding;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class FundingApplicationTests {
 
     @Test
