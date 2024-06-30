2e18a0c8ac2ec575c91240c11190b1d0a4396b75
강민정 <a50070863@gmail.com>
2024-06-30 12:12:27 +0900

    test: webMvcTest도 비활성화 (#4)

--- back/funding/src/test/java/com/ssafy/funding/controller/OrderControllerTest.java
+++ back/funding/src/test/java/com/ssafy/funding/controller/OrderControllerTest.java
@@ -19,13 +19,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 @AutoConfigureDataJpa
-@WebMvcTest(controllers = OrderControllerTest.class)
+// @WebMvcTest(controllers = OrderControllerTest.class)
 public class OrderControllerTest {
     @Autowired private MockMvc mockMvc;
     @Autowired private ObjectMapper objectMapper;
