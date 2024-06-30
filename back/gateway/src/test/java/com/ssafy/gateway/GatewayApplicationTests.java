5566fe94e9d29874247eb5138d549b477b15ee72
강민정 <a50070863@gmail.com>
2024-06-30 12:17:36 +0900

    test: gateway context load 테스트 비활성화 (#4)

--- back/gateway/src/test/java/com/ssafy/gateway/GatewayApplicationTests.java
+++ back/gateway/src/test/java/com/ssafy/gateway/GatewayApplicationTests.java
@@ -1,12 +1,14 @@
 package com.ssafy.gateway;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GatewayApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
