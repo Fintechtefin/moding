1a10b44ba4110380ced9c2f94c6bcef76d6b3cc3
minjeong <a50070863@gmail.com>
2024-07-02 14:21:30 +0900

    fix: import 구문 수정 (#3)
    패키지를 바꾸면서 import 구문이 수정이 안 된 부분이 있음

--- back/payment/src/main/java/com/ssafy/payment/domain/repository/PaymentCancelRepository.java
+++ back/payment/src/main/java/com/ssafy/payment/domain/repository/PaymentCancelRepository.java
@@ -3,4 +3,4 @@ package com.ssafy.payment.domain.repository;
 import com.ssafy.payment.domain.PaymentCancel;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PaymentCancelRepository extends CrudRepository<PaymentCancel, Integer> {}
+public interface PaymentCancelRepository extends CrudRepository<PaymentCancel, Long> {}
